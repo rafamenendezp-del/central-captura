5bb1b642d3520c85a5b08d188be4ab6cb2f50818
rafamenendezp-del <rafa.menendezp@gmail.com>
2026-08-19 18:06:00 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+   // Datos de conexion de CENTRAL. Se llena una vez y ya no se toca.
+   window.CENTRAL_URL  = "https://rbvegwvfmfqxbfovgcyb.supabase.co";
+   window.CENTRAL_ANON = "sb_publishable_ASUo91elaRUnxSmThrlT3g_rKqjZoAo";
